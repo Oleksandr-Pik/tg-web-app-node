e4d30f3d283bd2636618a4c0a4c66c02abdda2ea
Oleksandr-Pik <pik.aleksandr79@gmail.com>
2024-05-26 15:55:02 +0300

    some fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const TelegramBot = require("node-telegram-bot-api");
 
 const PORT = process.env.PORT;
@@ -42,13 +42,16 @@ bot.on("message", async (msg) => {
   if (msg?.web_app_data?.data) {
     try {
       const data = JSON.parse(msg?.web_app_data?.data);
-      console.log(data)
+      console.log(data);
       await bot.sendMessage(chatId, "Дякуємо за зворотній зв'язок!");
       await bot.sendMessage(chatId, "Ваше місто: " + data?.city);
       await bot.sendMessage(chatId, "Ваше вулиця: " + data?.street);
 
       setTimeout(async () => {
-        await bot.sendMessage(chatId, "Всю інформацію ви отримаєте в цьому чаті");
+        await bot.sendMessage(
+          chatId,
+          "Всю інформацію ви отримаєте в цьому чаті"
+        );
       }, 3000);
     } catch (error) {
       console.log(error.message);
@@ -56,27 +59,29 @@ bot.on("message", async (msg) => {
   }
 });
 
-app.post('/web', async (req, res) => {
-  const {queryId, products, totalPrice} = req.body;
+app.post("/web", async (req, res) => {
+  const { queryId, products, totalPrice } = req.body;
   try {
     await bot.answerWebAppQuery(queryId, {
-      type: 'article',
+      type: "article",
       id: queryId,
-      title: 'Вдала покупка',
-      input_message_content: {message_text: 'Вітаємо з покупкою, ви придбали товар на суму ' + totalPrice}
-    })
-    return res.status(200).json({})
-    
+      title: "Вдала покупка",
+      input_message_content: {
+        message_text: `Вітаємо з покупкою, ви придбали товар на суму ${totalPrice}, ${products
+          .map((item) => item.title)
+          .join(", ")}`,
+      },
+    });
+    return res.status(200).json({});
   } catch (error) {
     await bot.answerWebAppQuery(queryId, {
-      type: 'article',
+      type: "article",
       id: queryId,
-      title: 'Не вдалося придбати товар',
-      input_message_content: {message_text: 'Не вдалося придбати товар'}
-    })
-    return res.status(500).json({})
+      title: "Не вдалося придбати товар",
+      input_message_content: { message_text: "Не вдалося придбати товар" },
+    });
+    return res.status(500).json({});
   }
+});
 
-})
-
-app.listen(PORT, ()=> console.log('server started on PORT: ' + PORT));
+app.listen(PORT, () => console.log("server started on PORT: " + PORT));
